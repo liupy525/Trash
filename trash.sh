908e77e68796cb3d24c6553420a8add04f881545
liupy525 <liupy525@gmail.com>
2013-12-28 17:03:06 +0800

    修改了ts目录时无法建立info的bug

--- trash.sh
+++ trash.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 if [ x$1 != x ]; then
-    name=$1
+
+    if [ `echo $1 |grep /` ]; then
+        name=${1%?}
+    else
+        name=$1
+    fi
+    echo $name
     path=`pwd`
     date=`date +%Y-%m-%dT%H:%M:%S`
     trashpath=/home/liupy525/.Trash
     
-    if [ -f $trashpath/files/$1 ]; then
-        name=$1.`date +%Y-%m-%dT%H-%M-%S`
+    name_new=$name
+    if [ -f $trashpath/files/$name ]; then
+        name_new=$name.`date +%Y-%m-%dT%H-%M-%S`
     fi
     
-    if [ -d $trashpath/files/$1 ]; then
-        name=$1.`date +%Y-%m-%dT%H-%M-%S`
+    if [ -d $trashpath/files/$name ]; then
+        name_new=$name.`date +%Y-%m-%dT%H-%M-%S`
+        echo $name_new
     fi
     
-    mv $1 $trashpath/files/$name
-    echo "[Trash Info]" > $trashpath/info/$name.trashinfo
-    echo "Path=$path/$1" >> $trashpath/info/$name.trashinfo
-    echo "DeletionDate=$date" >> $trashpath/info/$name.trashinfo
+    mv $1 $trashpath/files/$name_new
+    echo "[Trash Info]" > $trashpath/info/$name_new.trashinfo
+    echo "Path=$path/$name" >> $trashpath/info/$name_new.trashinfo
+    echo "DeletionDate=$date" >> $trashpath/info/$name_new.trashinfo
 else
     echo "缺少参数！"
 fi
